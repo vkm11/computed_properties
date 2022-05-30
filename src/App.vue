f20ef097406ec9ad728abcae13cd48e898c15c47
vijaykumar <vijay1111mane@gmail.com>
2022-05-30 12:55:07 +0530

    commit;

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,23 @@
 <template>
   <h2>Full name - {{ firstName }} {{ lastName }}</h2>
   <h2>Computed fullName - {{ fullName }}</h2>
+  <button @click="changeFullName">Change Fullname</button> <!-- Computed setter -->
   <!-- <h2>
     Total - {{ items.reduce((total, curr) => (total = total + curr.price), 0) }}
   </h2> -->
   <button @click="items.push({id:4, title:'keybourd',price: 50})">Add Item</button>
   <h2>Computed Total - {{ total }}</h2>
+  <h2>Method Total - {{getTotal()}}</h2>
+  <input type="text" v-model="country">
+
+  <template v-for="item in items" :key="item.id">
+    <h2 v-if="item.price > 100">{{ item.title }} {{item.price}}</h2>
+  </template>
 </template>
+
+<h2 v-for='item in expensiveItems' :key='item.id'>
+    {{ item.title }} {{ item.price }}
+</h2>
 
 <script>
 export default {
@@ -32,21 +43,41 @@ export default {
           price: 300,
         },
       ],
-    };
+      country: ''
+    }
   },
-  methods: {},
+  methods: {
+    getTotal(){
+      console.log('getTotal Methods')
+      return this.items.reduce((total, curr) => (total = total + curr.price),0)
+    },
+    changeFullName(){   //computed setter
+      this.fullName = 'clark kent'
+    }
+  },
   computed: {
-    fullName() {
-      return `${this.firstName} ${this.lastName}`;
+    // fullName() {
+    //   return `${this.firstName} ${this.lastName}`
+    // },
+     fullName: {    //computed setter
+       get(){
+         return `${this.firstName} ${this.lastName}`
+       },
+      set(value){
+        const names = value.split(' ')
+        this.firstName = names[0],
+        this.lastName = names[1]
+      }
     },
     total() {
-      return this.items.reduce(
-        (total, curr) => (total = total + curr.price),
-        0
-      );
+      console.log("total computed properties")
+      return this.items.reduce((total, curr) => (total = total + curr.price),0)
     },
+    expensiveItems(){ 
+      return this.items.filter( item => item.price > 100)
+    }
   },
-};
+}
 </script>
 
 <style>
